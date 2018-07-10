0a5b78c58e068fff4ef6f278c9e6e62eadf3f428
oGuimaraes <otaviovgsr@gmail.com>
2018-07-10 14:19:20 -0300

    Matri format correction, indentation, exclusion commented code, class use in .css and ID only .js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var player = 1;
 var jogadas = 0;
 var ha_vencedor = false;
 var valida_posicao = [
-                      [false,false,false], // valida_posicao[0][0],valida_posicao[0][1],valida_posicao[0][2],
-                      [false,false,false], // valida_posicao[1][0],valida_posicao[1][1],valida_posicao[1][2],
-                      [false,false,false], // valida_posicao[2][0],valida_posicao[2][1],valida_posicao[2][2],
+    [false,false,false], // valida_posicao[0][0],valida_posicao[0][1],valida_posicao[0][2],
+    [false,false,false], // valida_posicao[1][0],valida_posicao[1][1],valida_posicao[1][2],
+    [false,false,false], // valida_posicao[2][0],valida_posicao[2][1],valida_posicao[2][2],
 ];
 
 var marca_posicao =  [
@@ -13,6 +13,7 @@ var marca_posicao =  [
     [false,false,false], 
 ];
 
+document.getElementById("vez").innerHTML = 'Vez de O';
 function clic(coluna,linha){
     
 console.log(valida_posicao[coluna][linha])
@@ -40,53 +41,6 @@ console.log(valida_posicao[coluna][linha])
 
     fimDeJogo();
 }
-
-/*  Player O CPU (Inacabado)
-
-function click(posicao_jogada){
-    
-    if(valida_posicao[posicao_jogada] == 0 && ha_vencedor == false){
-        if(player == 1) {
-            valida_posicao[posicao_jogada] = "bola";
-            document.getElementById("pos"+posicao_jogada).style.backgroundImage = 'url("images/bola.jpg")';
-            marca_posicao[posicao_jogada] = 'O';
-            player++;
-            jogadas++;
-            document.getElementById("vez").innerHTML = 'Vez de O';
-        } else {};
-    }   
-    fimDeJogo();
-
-    function vezCPU() {
-        if (player == 2){
-            var posCPU = Math.floor(Math.random() * 8)
-            console.log(posCPU+1);
-        
-            if (valida_posicao[posCPU] == false){
-                document.getElementById("pos"+posCPU).style.backgroundImage = 'url("images/Ois.jpg")';
-                document.getElementById("vez").innerHTML = 'Vez de O';
-                valida_posicao[posCPU] = "Ois";
-                marca_posicao[posicao_jogada] = 'O';
-                player--;
-                jogadas++;
-            }
-            else {
-                posCPU = Math.floor(Math.random() * 8)
-                document.getElementById("pos"+posCPU).style.backgroundImage = 'url("images/Ois.jpg")';
-                document.getElementById("vez").innerHTML = 'Vez de O';
-                valida_posicao[posCPU] = "Ois";
-                marca_posicao[posicao_jogada] = 'O';
-                player--;
-                jogadas++;    
-            }
-            fimDeJogo();
-            console.log(valida_posicao) 
-        }
-    }
-    vezCPU();
-}
-*/
-
 
 function fimDeJogo(){
 
